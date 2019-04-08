14522880cf6316bd8f75eea9e893bbbc6969c484
Shikhar0x1 <aoe0x1@gmail.com>
2019-04-08 06:03:59 +0530

    After Jenkins Setup

---
+++ foodie_django/apps/register/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+import bcrypt
+from models import User
+
+def index(request):
+    return render(request, 'register/home.html')
+
+def loginPage(request):
+    return render(request, 'register/index.html')
+
+def logout(request):
+    if (request.session['id']):
+            request.session['name'] = None
+            request.session['id'] = None
+    return redirect('/')
+
+def register(request):
+    errors = User.objects.validator(request.POST)
+    if len(errors):
+        for tag, error in errors.iteritems():
+            messages.error(request, error, extra_tags=tag)
+        return redirect('/')
+
+    hashed_password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+    user = User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], password=hashed_password, email=request.POST['email'])
+    user.save()
+    request.session['id'] = user.id
+    return redirect('/loginPage')
+
+def login(request):
+    if (User.objects.filter(email=request.POST['login_email']).exists()):
+        user = User.objects.filter(email=request.POST['login_email'])[0]
+        if (bcrypt.checkpw(request.POST['login_password'].encode(), user.password.encode())):
+            request.session['id'] = user.id
+            request.session['name'] = user.first_name
+            return redirect('/')
+    return redirect('/')
+
+def success(request):
+    user = User.objects.get(id=request.session['id'])
+    context = {
+        "user": user
+    }
+    return render(request, 'register/success.html', context)
+
+def viewProduct(request):
+    return render(request, 'register/viewProduct.html')
